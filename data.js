9908842fffb58192c2262562d084846302f06033
yurii <fetkoyura@gmail.com>
2020-07-14 18:32:30 +0300

    add structure folders and file data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1, name: "Kayak", category: "Watersports",
+        description: "A boat for one person", price: 275
+      },
+      {
+        id: 2, name: "Soccer Ball", category: "Soccer",
+        description: "FIFA-approved size and weight", price: 19.50
+      },
+      {
+        id: 3, name: "Human Chess Board", category: "Chess",
+        description: "A fun game for the family", price: 75
+      },
+    ],
+    orders: []
+  }
+}
